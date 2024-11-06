81117d6b0443f990522eae119f8e3c51c058f031
nightdentist <67285412+nightdentist@users.noreply.github.com>
2024-11-06 22:32:06 +0300

    Update coordinates_yourself.py

--- coordinates_yourself.py
+++ coordinates_yourself.py
@@ -1,6 +1,7 @@
 import requests
 import os
 
+from geopy import distance
 from dotenv import load_dotenv
 
 
@@ -19,14 +20,20 @@ def fetch_coordinates(apikey, address):
 
     most_relevant = found_places[0]
     lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lon, lat
+    return lat, lon
 
 
 def main():
     load_dotenv()
     apikey = os.getenv("KEY_YANDEX")
-    address = input("Где вы находитесь ? ")
-    print("Ваши координаты: ", fetch_coordinates(apikey, address))
+    #address = input("Где вы находитесь ? ")
+    point_1 = input("Пункт А: ")
+    point_2 = input("Пункт Б: ")
+    coord_point_1 = fetch_coordinates(apikey, point_1)
+    print(coord_point_1)
+    coord_point_2 = fetch_coordinates(apikey, point_2)
+    #print("Ваши координаты: ", fetch_coordinates(apikey, address))
+    print("Расстояние: ", distance.distance(coord_point_1, coord_point_2).km)
 
 
 if __name__ == '__main__':
