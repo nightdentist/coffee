cd0992b7d21dfd5d7bee6a11c1fde2713b89a0fa
nightdentist <67285412+nightdentist@users.noreply.github.com>
2024-11-08 11:44:39 +0300

    new_correction

--- coordinates_yourself.py
+++ coordinates_yourself.py
@@ -1,8 +1,24 @@
 import requests
 import os
+import json
 
 from geopy import distance
 from dotenv import load_dotenv
+
+
+def ccoffee_all():
+    with open("coffee.json", "r", encoding="CP1251") as my_file:
+        coffee_json = my_file.read()
+
+    coffee = json.loads(coffee_json)
+    #first_coffee = coffee[0]['Name'], coffee[0]['geoData']['coordinates'][0], coffee[0]['geoData']['coordinates'][1]
+
+    index = 0
+
+    for i in coffee:
+        result = coffee[index]['Name'], coffee[0]['geoData']['coordinates'][0], coffee[0]['geoData']['coordinates'][1]
+        index = index + 1
+        return result
 
 
 def fetch_coordinates(apikey, address):
@@ -26,13 +42,13 @@ def fetch_coordinates(apikey, address):
 def main():
     load_dotenv()
     apikey = os.getenv("KEY_YANDEX")
-    #address = input("Где вы находитесь ? ")
-    point_1 = input("Пункт А: ")
+    address = input("Где вы находитесь ? ")
+    point_1 = address
     point_2 = input("Пункт Б: ")
     coord_point_1 = fetch_coordinates(apikey, point_1)
     print(coord_point_1)
     coord_point_2 = fetch_coordinates(apikey, point_2)
-    #print("Ваши координаты: ", fetch_coordinates(apikey, address))
+    print("Ваши координаты: ", fetch_coordinates(apikey, address))
     print("Расстояние: ", distance.distance(coord_point_1, coord_point_2).km)
 
 
