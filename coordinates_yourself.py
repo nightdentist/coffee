2e77207aa84633a40378ed485b2bc9d3af7cade2
nightdentist <67285412+nightdentist@users.noreply.github.com>
2024-11-13 23:19:20 +0300

    new_commit

--- coordinates_yourself.py
+++ coordinates_yourself.py
@@ -6,15 +6,11 @@ from geopy import distance
 from dotenv import load_dotenv
 
 
-def ccoffee_all():
+def coffee_all():
     with open("coffee.json", "r", encoding="CP1251") as my_file:
         coffee_json = my_file.read()
-
     coffee = json.loads(coffee_json)
-    #first_coffee = coffee[0]['Name'], coffee[0]['geoData']['coordinates'][0], coffee[0]['geoData']['coordinates'][1]
-
     index = 0
-
     for i in coffee:
         result = coffee[index]['Name'], coffee[0]['geoData']['coordinates'][0], coffee[0]['geoData']['coordinates'][1]
         index = index + 1
@@ -44,12 +40,11 @@ def main():
     apikey = os.getenv("KEY_YANDEX")
     address = input("Где вы находитесь ? ")
     point_1 = address
-    point_2 = input("Пункт Б: ")
+    point_2 = coffee_all
     coord_point_1 = fetch_coordinates(apikey, point_1)
-    print(coord_point_1)
     coord_point_2 = fetch_coordinates(apikey, point_2)
-    print("Ваши координаты: ", fetch_coordinates(apikey, address))
-    print("Расстояние: ", distance.distance(coord_point_1, coord_point_2).km)
+    print("Ваши координаты: ", coord_point_1)
+    print("Расстояние :", str(distance.distance(coord_point_1, coord_point_2).km))
 
 
 if __name__ == '__main__':
