145b92200b9c8f26bafbec03e5bb1bc3fc1d73fa
nightdentist <67285412+nightdentist@users.noreply.github.com>
2024-11-16 22:07:03 +0300

    Update coordinates_yourself.py

--- coordinates_yourself.py
+++ coordinates_yourself.py
@@ -2,19 +2,32 @@ import requests
 import os
 import json
 
+from pprint import pprint
 from geopy import distance
 from dotenv import load_dotenv
 
 
-def coffee_all():
+def get_nearest_coffee(distance_coffees):
+    return distance_coffees['distance']
+
+
+def coffee_all(coord_point_1):
     with open("coffee.json", "r", encoding="CP1251") as my_file:
         coffee_json = my_file.read()
-    coffee = json.loads(coffee_json)
-    index = 0
-    for i in coffee:
-        result = coffee[index]['Name'], coffee[0]['geoData']['coordinates'][0], coffee[0]['geoData']['coordinates'][1]
-        index = index + 1
-        return result
+    all_coffees = json.loads(coffee_json)
+    list_coffees = []
+    for coffee in all_coffees:
+        name = coffee['Name']
+        lon = coffee['geoData']['coordinates'][0]
+        lat = coffee['geoData']['coordinates'][1]
+        coord_point_2 = lon, lat
+        dict_coffees = dict()
+        dict_coffees['title'] = name
+        dict_coffees['distance'] = str(distance.distance(coord_point_1, coord_point_2).km)
+        dict_coffees['longitude'] = lon
+        dict_coffees['latitude'] = lat
+        list_coffees.append(dict_coffees)
+    return list_coffees
 
 
 def fetch_coordinates(apikey, address):
@@ -32,7 +45,7 @@ def fetch_coordinates(apikey, address):
 
     most_relevant = found_places[0]
     lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lat, lon
+    return lon, lat
 
 
 def main():
@@ -40,11 +53,11 @@ def main():
     apikey = os.getenv("KEY_YANDEX")
     address = input("Где вы находитесь ? ")
     point_1 = address
-    point_2 = coffee_all
     coord_point_1 = fetch_coordinates(apikey, point_1)
-    coord_point_2 = fetch_coordinates(apikey, point_2)
+    distanse_to_coffees = coffee_all(coord_point_1)
     print("Ваши координаты: ", coord_point_1)
-    print("Расстояние :", str(distance.distance(coord_point_1, coord_point_2).km))
+    m = sorted(distanse_to_coffees, key=get_nearest_coffee)
+    pprint(m)
 
 
 if __name__ == '__main__':
